b7f5bf7ce3c42e5522e185ca93f1596c0d2375e5
Laurent Zeimes <laurent@alphaticks.io>
2025-01-15 22:51:56 +0100

    feat(grpc): rate limit specs

--- hypurr.go
+++ hypurr.go
@@ -15,3 +15,76 @@ func (x *HyperliquidLaunchTradeRequest) TelegramID() int64 {
 	}
 	return userId
 }
+
+// Rate limit weights
+func (x *HyperliquidDeployAuctionRequest) Weight() int {
+	return 1
+}
+
+func (x *HyperliquidTokensRequest) Weight() int {
+	return 1
+}
+
+func (x *HyperliquidTokenHoldersRequest) Weight() int {
+	return 5
+}
+
+func (x *HyperliquidTokenMessagesRequest) Weight() int {
+	return 2
+}
+
+func (x *HyperliquidSpotPairRequest) Weight() int {
+	return 1
+}
+
+func (x *HyperliquidSpotPairsRequest) Weight() int {
+	return 2
+}
+
+func (x *HyperliquidPerpPairsRequest) Weight() int {
+	return 2
+}
+
+func (x *HyperliquidWalletRequest) Weight() int {
+	return 2
+}
+
+func (x *HyperliquidWalletPerformanceRequest) Weight() int {
+	return 10
+}
+
+func (x *HyperliquidLaunchRequest) Weight() int {
+	return 1
+}
+
+func (x *HyperliquidLaunchesRequest) Weight() int {
+	return 2
+}
+
+func (x *HyperliquidLaunchStreamRequest) Weight() int {
+	return 2
+}
+
+func (x *HyperliquidLaunchFillsRequest) Weight() int {
+	return 2
+}
+
+func (x *LatestHyperliquidLaunchFillsRequest) Weight() int {
+	return 2
+}
+
+func (x *HyperliquidLaunchCandlesRequest) Weight() int {
+	return 2
+}
+
+func (x *HyperliquidLaunchMessagesRequest) Weight() int {
+	return 2
+}
+
+func (x *HyperliquidLaunchHoldersRequest) Weight() int {
+	return 5
+}
+
+func (x *HypurrFunCabalsRequest) Weight() int {
+	return 2
+}
