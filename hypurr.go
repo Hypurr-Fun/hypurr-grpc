b9cf095a55327025447f3980fbea2fbc37765ad5
Laurent Zeimes <laurent@alphaticks.io>
2025-01-15 22:54:42 +0100

    feat(grpc): rate limit specs

--- hypurr.go
+++ hypurr.go
@@ -17,74 +17,74 @@ func (x *HyperliquidLaunchTradeRequest) TelegramID() int64 {
 }
 
 // Rate limit weights
-func (x *HyperliquidDeployAuctionRequest) Weight() int {
+func (x *HyperliquidDeployAuctionRequest) Weight() uint32 {
 	return 1
 }
 
-func (x *HyperliquidTokensRequest) Weight() int {
+func (x *HyperliquidTokensRequest) Weight() uint32 {
 	return 1
 }
 
-func (x *HyperliquidTokenHoldersRequest) Weight() int {
+func (x *HyperliquidTokenHoldersRequest) Weight() uint32 {
 	return 5
 }
 
-func (x *HyperliquidTokenMessagesRequest) Weight() int {
+func (x *HyperliquidTokenMessagesRequest) Weight() uint32 {
 	return 2
 }
 
-func (x *HyperliquidSpotPairRequest) Weight() int {
+func (x *HyperliquidSpotPairRequest) Weight() uint32 {
 	return 1
 }
 
-func (x *HyperliquidSpotPairsRequest) Weight() int {
+func (x *HyperliquidSpotPairsRequest) Weight() uint32 {
 	return 2
 }
 
-func (x *HyperliquidPerpPairsRequest) Weight() int {
+func (x *HyperliquidPerpPairsRequest) Weight() uint32 {
 	return 2
 }
 
-func (x *HyperliquidWalletRequest) Weight() int {
+func (x *HyperliquidWalletRequest) Weight() uint32 {
 	return 2
 }
 
-func (x *HyperliquidWalletPerformanceRequest) Weight() int {
+func (x *HyperliquidWalletPerformanceRequest) Weight() uint32 {
 	return 10
 }
 
-func (x *HyperliquidLaunchRequest) Weight() int {
+func (x *HyperliquidLaunchRequest) Weight() uint32 {
 	return 1
 }
 
-func (x *HyperliquidLaunchesRequest) Weight() int {
+func (x *HyperliquidLaunchesRequest) Weight() uint32 {
 	return 2
 }
 
-func (x *HyperliquidLaunchStreamRequest) Weight() int {
+func (x *HyperliquidLaunchStreamRequest) Weight() uint32 {
 	return 2
 }
 
-func (x *HyperliquidLaunchFillsRequest) Weight() int {
+func (x *HyperliquidLaunchFillsRequest) Weight() uint32 {
 	return 2
 }
 
-func (x *LatestHyperliquidLaunchFillsRequest) Weight() int {
+func (x *LatestHyperliquidLaunchFillsRequest) Weight() uint32 {
 	return 2
 }
 
-func (x *HyperliquidLaunchCandlesRequest) Weight() int {
+func (x *HyperliquidLaunchCandlesRequest) Weight() uint32 {
 	return 2
 }
 
-func (x *HyperliquidLaunchMessagesRequest) Weight() int {
+func (x *HyperliquidLaunchMessagesRequest) Weight() uint32 {
 	return 2
 }
 
-func (x *HyperliquidLaunchHoldersRequest) Weight() int {
+func (x *HyperliquidLaunchHoldersRequest) Weight() uint32 {
 	return 5
 }
 
-func (x *HypurrFunCabalsRequest) Weight() int {
+func (x *HypurrFunCabalsRequest) Weight() uint32 {
 	return 2
 }
