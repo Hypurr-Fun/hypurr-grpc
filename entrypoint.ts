abcfad2b0ea93d39b491bdb1ae29f6ceee3eee5c
Laurent Zeimes <laurent@alphaticks.io>
2025-01-27 12:41:47 +0100

    chore(grpc): refactoring

--- entrypoint.ts
+++ entrypoint.ts
@@ -2,9 +2,11 @@ export * from "./ts/hypurr/cabal";
 export * from "./ts/hypurr/common";
 export * from "./ts/hypurr/launch";
 export * from "./ts/hypurr/pairs";
-export * from "./ts/hypurr/services";
 export * from "./ts/hypurr/token";
 export * from "./ts/hypurr/user";
 export * from "./ts/hypurr/wallet";
-export * from "./ts/hypurr/services";
-export * from "./ts/hypurr/services.client";
+export * from "./ts/hypurr/static_service";
+export * from "./ts/hypurr/static_service.client";
+export * from "./ts/hypurr/telegram_service";
+export * from "./ts/hypurr/telegram_service.client";
+
