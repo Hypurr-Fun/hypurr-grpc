0e8520ee3b200175a5e1452103e370cca419d350
Laurent Zeimes <laurent@alphaticks.io>
2025-01-27 14:26:30 +0100

    feat(grpc): include tools in ts

--- entrypoint.ts
+++ entrypoint.ts
@@ -5,6 +5,7 @@ export * from "./ts/hypurr/pairs";
 export * from "./ts/hypurr/token";
 export * from "./ts/hypurr/user";
 export * from "./ts/hypurr/wallet";
+export * from "./ts/hypurr/tools";
 export * from "./ts/hypurr/static_service";
 export * from "./ts/hypurr/static_service.client";
 export * from "./ts/hypurr/telegram_service";
