1fc96637a667bcde8ad073cc9efef2189bdb4af4
Laurent Zeimes <laurent@alphaticks.io>
2025-01-30 15:44:38 +0100

    chore(grpc): package path

--- python/hypurr/common_pb2.py
+++ python/hypurr/common_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='hypurr/common.proto',
   package='hypurr',
   syntax='proto3',
-  serialized_pb=_b('\n\x13hypurr/common.proto\x12\x06hypurr\"e\n\x12TelegramUserPublic\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x10\n\x08username\x18\x02 \x01(\t\x12\x17\n\x0fpicture_file_id\x18\x03 \x01(\t\x12\x18\n\x10reputation_score\x18\x04 \x01(\x03\x42-Z+gitlab.com/hypurr-fun/hypurr-grpc/go/hypurrb\x06proto3')
+  serialized_pb=_b('\n\x13hypurr/common.proto\x12\x06hypurr\"e\n\x12TelegramUserPublic\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x10\n\x08username\x18\x02 \x01(\t\x12\x17\n\x0fpicture_file_id\x18\x03 \x01(\t\x12\x18\n\x10reputation_score\x18\x04 \x01(\x03\x42)Z\'gitlab.com/hypurr/hypurr-grpc/go/hypurrb\x06proto3')
 )
 
 
@@ -88,5 +88,5 @@ _sym_db.RegisterMessage(TelegramUserPublic)
 
 
 DESCRIPTOR.has_options = True
-DESCRIPTOR._options = _descriptor._ParseOptions(descriptor_pb2.FileOptions(), _b('Z+gitlab.com/hypurr-fun/hypurr-grpc/go/hypurr'))
+DESCRIPTOR._options = _descriptor._ParseOptions(descriptor_pb2.FileOptions(), _b('Z\'gitlab.com/hypurr/hypurr-grpc/go/hypurr'))
 # @@protoc_insertion_point(module_scope)
