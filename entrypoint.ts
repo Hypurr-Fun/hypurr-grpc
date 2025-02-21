b16cb72f5a4657104599f8375eea2fd62c3cb860
Laurent Zeimes <laurent@alphaticks.io>
2025-02-21 11:37:57 +0100

    feat(grpc): evm

--- entrypoint.ts
+++ entrypoint.ts
@@ -6,9 +6,11 @@ export * from "./ts/hypurr/token";
 export * from "./ts/hypurr/user";
 export * from "./ts/hypurr/wallet";
 export * from "./ts/hypurr/tools";
+export * from "./ts/hypurr/evm";
 export * from "./ts/hypurr/static_service";
 export * from "./ts/hypurr/static_service.client";
 export * from "./ts/hypurr/telegram_service";
 export * from "./ts/hypurr/telegram_service.client";
-
+export * from "./ts/hypurr/evm_service";
+export * from "./ts/hypurr/evm_service.client";
 
