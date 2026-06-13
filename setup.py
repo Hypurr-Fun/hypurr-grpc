32e29509c84f4caa904abebd6eea0fdb134703f9
Gosthell <example@example.com>
2026-06-13 18:04:45 +0200

    feat(grpc): portfolio allocator

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from setuptools import setup
+from setuptools import find_namespace_packages, setup
 
 setup(
-    name='hypurr-grpc',
-    version='1.0.3',
-    packages=[''],
-    url='https://gitlab.com/hypurr/hypurr-grpc',
-    license='copyright',
-    author='Hypurr Fun LLP',
-    description='gRPC code for hypurr client'
+    name="hypurr-grpc",
+    version="1.0.3",
+    packages=find_namespace_packages(where="python"),
+    package_dir={"": "python"},
+    install_requires=[
+        "grpcio>=1.76,<2",
+        "protobuf>=6.33.5,<7",
+    ],
+    url="https://github.com/Hypurr-Fun/hypurr-grpc",
+    license="copyright",
+    author="Hypurr Fun LLP",
+    description="gRPC code for hypurr client",
 )
