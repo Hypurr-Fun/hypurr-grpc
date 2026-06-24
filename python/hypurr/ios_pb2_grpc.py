7f037c0012bc3099cc02ffb3f968fbb5631f7f35
Gosthell <example@example.com>
2026-06-24 12:29:23 +0200

    feat(user): address book

--- python/hypurr/ios_pb2_grpc.py
+++ python/hypurr/ios_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from hypurr import ios_pb2 as hypurr_dot_ios__pb2
 
-GRPC_GENERATED_VERSION = '1.80.0'
+GRPC_GENERATED_VERSION = '1.81.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
@@ -25,7 +25,7 @@ if _version_not_supported:
     )
 
 
-class HomePortfolioServiceStub(object):
+class HomePortfolioServiceStub:
     """Missing associated documentation comment in .proto file."""
 
     def __init__(self, channel):
@@ -41,7 +41,7 @@ class HomePortfolioServiceStub(object):
                 _registered_method=True)
 
 
-class HomePortfolioServiceServicer(object):
+class HomePortfolioServiceServicer:
     """Missing associated documentation comment in .proto file."""
 
     def PortfolioSummary(self, request, context):
@@ -66,7 +66,7 @@ def add_HomePortfolioServiceServicer_to_server(servicer, server):
 
 
  # This class is part of an EXPERIMENTAL API.
-class HomePortfolioService(object):
+class HomePortfolioService:
     """Missing associated documentation comment in .proto file."""
 
     @staticmethod
